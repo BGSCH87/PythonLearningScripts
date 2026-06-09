213ea36e3ccdd50d4e9b80fd369749c4b5aa9bb1
Bastiaan Schreuder <bastiaanschreuder@gmail.com>
2026-06-09 16:49:52 +0200

    some changing to make the code work better

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,35 +1,40 @@
 from random import randint
 
-text = input("Enter the message you would like to encipher: ")
-text = text.upper()
-shift = randint(1, 25)
-
 
 def caesar_cipher_up(text, shift):
+    encoded_text = ""
+    for char in text:
+        if char.isalpha():
+            encoded_text += chr((ord(char) - 65 + shift) % 26 + 65)
+        else:
+            encoded_text += char
+
+    print(f"Encoded Message: {encoded_text}")
+    return encoded_text
+
+
+def caesar_cipher_down(text, shift):
     decoded_text = ""
     for char in text:
         if char.isalpha():
-            decoded_text += chr((ord(char) - 65 + shift) % 26 + 65)
+            decoded_text += chr((ord(char) - 65 - shift) % 26 + 65)
         else:
             decoded_text += char
 
     print(f"Decoded Message: {decoded_text}")
+    return decoded_text
 
 
-def ceasar_cipher_down(text, shift):
-    decoded_text = ""
-    for char in text:
-        if char.isalpha():
-            decoded_text += chr((ord(char) - 65 + (26 + shift)) % 26 + 65)
-        else:
-            decoded_text += char
+if __name__ == "__main__":
+    text = input("Enter the message you would like to encipher: ")
+    text = text.upper()
+    shift = randint(1, 25)
 
-    print(f"Decoded Message: {decoded_text}")
-
-
-print(
-    f"Your original message is {text}. We will now use a random to int to Decipher the message"
-)
-caesar_cipher_up(text, shift)
-print(f"Enigma value: {shift} \nWe will use this value to decipher the message")
-ceasar_cipher_down(text, shift)
+    print(
+        f"Your original message is {text}. We will now use a random int (KEY) to decipher the message"
+    )
+    encoded = caesar_cipher_up(text, shift)
+    print(
+        f"Enigma value: {shift} \nWe will use this KEY ({shift}) to decipher the message"
+    )
+    caesar_cipher_down(encoded, shift)
